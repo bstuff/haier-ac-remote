38fbbcaa8c84b06abdce3b140cba25bb947964e0
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2020-06-02 19:43:30 +0300

    fix haier-ac-remote example (#9)

--- packages/haier-ac-remote/src/__tests__/commands.test.ts
+++ packages/haier-ac-remote/src/__tests__/commands.test.ts
@@ -4,7 +4,7 @@ import * as commands from '../lib/commands';
 
 describe('commands', () => {
   it('should work', () => {
-    const a = commands.on();
+    const a = commands.on('0007A8E917AC');
 
     expect(hexy(a)).toMatchSnapshot();
   });
